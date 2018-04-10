ae19aef3a0dd0d5bb163f0c580c8a0ca07a9464b
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 18:21:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@ def prime?(integer)
   
   if integer == 2 || 3 || 5 || 7 
     return true
-    elsif integer % 2 != 0 && integer % 3 != 0 && integer % 5 != 0 && integer % 7 != 0
-    return true
     elsif integer <= 1
     return false
+    elsif integer % 2 != 0 && integer % 3 != 0 && integer % 5 != 0 && integer % 7 != 0
+    return true
   else
     return false
   end
