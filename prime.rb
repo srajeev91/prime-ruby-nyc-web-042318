e74cd92bef8510623531fcadc571f9482eba607a
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-10 18:36:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,20 @@
 def prime?(num)
   if num <= 1 
     return false
+  
   elsif num == 2 
     return true
+  
   else 
   i = 2
-  while i < num
-    if num % i == 0
-      return false
+    while i < num
+      if num % i == 0
+        return false
+      end
+      i += 1
     end
-  i += 1
-end
-end
-return true
+  end
+  
+  return true
+  
 end
